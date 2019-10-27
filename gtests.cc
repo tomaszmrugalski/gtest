c7f878b19d42154b565803b2481dd1df16949126
Tomek Mrugalski <tomasz@isc.org>
2019-10-27 23:55:41 +0100

    Basic factorial test added

--- gtests.cc
+++ gtests.cc
@@ -9,7 +9,11 @@ TEST(math, add) {
     EXPECT_EQ(5, sum(2,3));
 }
 
-TEST(factorial, basic) {
+TEST(factorial, simple) {
+    EXPECT_EQ(120, fac(5));
+}
+
+TEST(factorial, negative) {
     EXPECT_NO_THROW(fac(5));
 
     EXPECT_THROW(fac(-5), std::out_of_range);
